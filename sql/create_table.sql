eca915938eb426862aa54bbb15b7d51c9bb17c1e
Ismayil Khayredinov <ismayil.khayredinov@gmail.com>
2015-02-17 18:50:05 +0100

    Remove comments

--- sql/create_table.sql
+++ sql/create_table.sql
@@ -1,11 +1,3 @@
---CREATE TABLE IF NOT EXISTS `prefix_entity_geometry` (
---  `id` int(11) NOT NULL AUTO_INCREMENT,
---  `entity_guid` bigint(20) unsigned NOT NULL,
---  `geom` GEOMETRY NOT NULL,
---  PRIMARY KEY (`id`),
---  UNIQUE KEY `entity_guid` (`entity_guid`),
---  SPATIAL KEY `geometry` (`geometry`)
---) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 CREATE TABLE IF NOT EXISTS `prefix_entity_geometry` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `entity_guid` bigint(20) unsigned NOT NULL,
